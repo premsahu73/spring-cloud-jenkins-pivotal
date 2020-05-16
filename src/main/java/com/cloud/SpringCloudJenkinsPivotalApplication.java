d3f410ec85b4fcc85fa5cf9ed6bce07a738b748e
unknown <premprasadsaw73@gmail.com>
2020-05-16 17:15:54 +0530

    first commit

---
+++ src/main/java/com/cloud/SpringCloudJenkinsPivotalApplication.java
@@ -0,0 +1,13 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudJenkinsPivotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudJenkinsPivotalApplication.class, args);
+	}
+
+}
